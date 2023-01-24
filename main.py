94d420e29b73243d67f699c32e3bc70bb6e3db88
gavincggit <gavincggit@gmail.com>
2023-01-24 21:05:31 +0000

    big commit

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from typing import Union
+import psycopg2
 
 from fastapi import FastAPI
 
 app = FastAPI()
 
+connection = psycopg2.connect(
+    host="localhost",
+    database="psycopgtest",
+    user="postgres",
+    password=None,
+)
+connection.set_session(autocommit=True)
 
 @app.get("/")
 def read_root():
@@ -13,3 +21,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+with connection.cursor() as cursor:
+    cursor.execute('SELECT COUNT(*) FROM account')
+    result = cursor.fetchone()
+    print(result)
